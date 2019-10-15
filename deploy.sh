fb85ac35fa2bf5e598b34e27d517dd7a4b838533
zhangsanshi <zhangsanshi5566@gmail.com>
2019-10-15 13:48:22 +0800

    update repo url

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f https://github.com/vusion/cloud-admin-site.git master:gh-pages
+git push -f https://github.com/vusion-templates/cloud-admin-site.git master:gh-pages
 
 cd -
