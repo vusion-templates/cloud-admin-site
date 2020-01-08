462611b449fa2966df64d4b6ead09ff587144219
zhangsanshi <zhangsanshi5566@gmail.com>
2020-01-08 14:38:17 +0800

    support iframe redirect

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,8 +1,29 @@
+import message from './message';
 export default ({
-    Vue, // the version of Vue being used in the VuePress app
-    options, // the options for the root Vue instance
-    router, // the router instance for the app
-    siteData // site metadata
-  }) => {
-    // ...apply enhancements to the app
-  }
+  Vue, // the version of Vue being used in the VuePress app
+  options, // the options for the root Vue instance
+  router, // the router instance for the app
+  siteData // site metadata
+}) => {
+  new Vue({
+    data() {
+      return {
+        message,
+      };
+    },
+    watch: {
+      'message.route'(route) {
+        if (route) {
+          if (!this.ready) {
+            this.ready = true;
+            router.onReady(() => {
+              router.replace(this.message.route);
+            });
+          } else {
+            router.replace(route);
+          }
+        }
+      },
+    },
+  });
+}
